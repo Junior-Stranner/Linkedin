2583ebd05822d36a791cfe24377dc3ab8406e50a
Junior-Stranner <JuniorStranner@gmail.com>
2025-04-29 09:43:35 -0300

    add security, gonna use jjwt library

---
+++ Backend/src/main/java/br/com/judev/backend/feature/authentication/service/EmailService.java
@@ -0,0 +1,32 @@
+package br.com.judev.backend.feature.authentication.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String email, String subject, String content) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setFrom("no-reply@linkedin.com");
+        helper.setTo(email);
+
+        helper.setSubject(subject);
+        helper.setText(content, true);
+
+        mailSender.send(message);
+
+    }
+}
